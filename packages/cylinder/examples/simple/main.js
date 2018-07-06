7db318d8d88db5a910492d8aeb3c1165d4e47c1d
张磊 <kmdjs@qq.com>
2018-07-06 10:42:38 +0800

    add cylinder demo

---
+++ packages/cylinder/examples/simple/main.js
@@ -0,0 +1,31 @@
+import cax from 'cax'
+import Cylinder from '../../src/index'
+
+const stage = new cax.Stage(700, 420, '#canvasCtn')
+
+
+
+const colors = ['#2781BD', '#22A387', '#9ABC5B', '#F59D1F', '#BF3A2D']
+
+function random(){
+    stage.empty()
+    for (let i = 0; i < 5; i++) {
+        let cy = new Cylinder(60, 200, Math.random(), colors[i])
+        cy.x = 100 + 100 * i
+        cy.y = 100
+
+        let text  = new cax.Text(Math.round(cy.value*100)+'%',{
+            font:'20px Arial',
+            color: 'white'
+        })
+        text.x = cy.x +cy.width/2- text.getWidth()/2
+        text.y = cy.y + cy.height - cy.height * cy.value
+        stage.add(cy,text)
+    }
+}
+
+random()
+
+cax.tick(stage.update.bind(stage))
+
+document.querySelector('#randomBtn').addEventListener('click', random)
