5bee165cf24a401249a8afb37aee279c9c838fb6
张磊 <kmdjs@qq.com>
2018-07-12 17:57:46 +0800

    add pyramid

---
+++ packages/pyramid3d/examples/simple/main.js
@@ -0,0 +1,49 @@
+import Pyramid from '../../src/index'
+import './orbit-controls.js'
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 500)
+camera.position.set(0, 20, 210)
+
+const scene = new THREE.Scene()
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0xAAAAAA)
+document.body.appendChild(renderer.domElement)
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+const group = new THREE.Group()
+
+const pyramid = new Pyramid({
+  items:[{
+    value:5,
+    color:'#DE5347'
+  },{
+    value:20,
+    color:'#3DCE3D'
+  },{
+    value:10,
+    color:'#0080FF'
+  }],
+  text:(item)=>{
+    return 'value-'+item.value
+  },
+  height: 20,
+  interval: 2
+})
+group.add(pyramid)
+scene.add(group)
+
+
+// light
+const light = new THREE.PointLight(0xffffff, 1, 1000)
+light.position.set(0, 10, 100)
+scene.add(light)
+
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  controls.update();	
+}
+
+animate()
---
+++ packages/pyramid3d/src/index.js
@@ -0,0 +1,65 @@
+import font from './font'
+
+class Pyramid extends THREE.Group {
+  constructor(option) {
+
+    super()
+
+    this.option = option
+
+    this.option.items.sort((a,b)=>{
+      return a.value-b.value
+    })
+
+    this.height = this.option.height / this.option.items.length
+
+    let currentR = 0,
+      nextR = this.height,
+      currentHeight = 0
+
+
+    this.option.items.forEach((item, index) => {
+      var geometry = new THREE.CylinderGeometry(currentR, nextR, this.height, 32);
+
+      var material = new THREE.MeshPhongMaterial({ color: item.color });
+      var cylinder = new THREE.Mesh(geometry, material);
+      cylinder.position.y -= currentHeight + this.option.interval * index
+     
+      this.add(cylinder)
+
+      this.addText(this.option.text(item),(currentR+nextR)/2, cylinder.position.y,0)
+
+      currentHeight += this.height
+      currentR += this.height
+      nextR += this.height
+
+    })
+
+
+    
+  }
+
+
+  addText(text, x, y, z) {
+    const gem = new THREE.TextGeometry(text + '', {
+      font: font, //字体，默认是'helvetiker'，需对应引用的字体文件
+      size: 2, //字号大小，一般为大写字母的高度
+      height: 0.1
+    });
+
+    const mat = new THREE.MeshPhongMaterial({
+      color: 0x000000,
+    });
+
+    //gem.left()
+    const textObj = new THREE.Mesh(gem, mat);
+
+    textObj.position.x = x
+    textObj.position.y = y
+    textObj.position.z = z
+
+    this.add(textObj)
+  }
+}
+
+export default Pyramid
