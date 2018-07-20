471998bd802b8e7755fd957180a296b5bc3b8912
张磊 <kmdjs@qq.com>
2018-07-20 09:48:35 +0800

    add avatar

---
+++ packages/avatar/examples/simple/main.js
@@ -0,0 +1,124 @@
+
+import cax from 'cax'
+import Loader from '../../../common/loader'
+//import Axis from '../../src/index'
+
+
+const stage = new cax.Stage(240, 280, '#canvasCtn')
+
+const loader = new Loader({
+  res: [{ id: 'a1', src: '../../asset/a1.jpg' }, { id: 'a2', src: '../../asset/a2.jpg' }, { id: 'a3', src: '../../asset/a3.jpg' }],
+  complete: () => {
+
+
+    let groupA = new cax.Group()
+    let bitmapA = new cax.Bitmap(loader.get('a1'))
+
+    let clipPathA = new cax.Graphics()
+    clipPathA.x = 70
+    clipPathA.y = 70
+    clipPathA.arc(0, 0, 55, 0, Math.PI * 2)
+    bitmapA.clip(clipPathA)
+    let graphicsA = new cax.Graphics()
+
+    graphicsA.beginPath().strokeStyle('white').lineWidth(2).arc(0, 0, 55, 0, Math.PI * 2).stroke()
+
+    graphicsA.x = 70
+    graphicsA.y = 70
+    groupA.add(bitmapA, graphicsA)
+
+    stage.add(groupA)
+
+
+
+    let groupB = new cax.Group()
+    let bitmapB = new cax.Bitmap(loader.get('a2'))
+
+    let clipPathB = new cax.Graphics()
+    clipPathB.x = 70
+    clipPathB.y = 70
+    clipPathB.arc(0, 0, 55, 0, Math.PI * 2)
+    bitmapB.clip(clipPathB)
+    let graphicsB = new cax.Graphics()
+
+    graphicsB.beginPath().strokeStyle('white').lineWidth(2).arc(0, 0, 55, 0, Math.PI * 2).stroke()
+
+    graphicsB.x = 70
+    graphicsB.y = 70
+    groupB.add(bitmapB, graphicsB)
+    groupB.x = 100
+
+    stage.add(groupB)
+
+
+
+
+
+
+    const group = new cax.Group()
+
+    groupB = new cax.Group()
+    bitmapB = new cax.Bitmap(loader.get('a3'))
+
+    clipPathB = new cax.Graphics()
+    clipPathB.x = 70
+    clipPathB.y = 70
+    clipPathB.arc(0, 0, 55, 0, Math.PI * 2)
+    bitmapB.clip(clipPathB)
+    graphicsB = new cax.Graphics()
+
+    graphicsB.beginPath().strokeStyle('white').lineWidth(2).arc(0, 0, 55, 0, Math.PI * 2).stroke()
+
+    graphicsB.x = 70
+    graphicsB.y = 70
+    groupB.add(bitmapB, graphicsB)
+    groupB.x = 50
+    groupB.y = 290
+
+    group.add(groupB)
+
+
+    groupA = new cax.Group()
+    bitmapA = new cax.Bitmap(loader.get('a1'))
+
+    clipPathA = new cax.Graphics()
+    clipPathA.x = 70
+    clipPathA.y = 70
+    clipPathA.arc(0, 0, 55, 0, Math.PI * 2)
+    bitmapA.clip(clipPathA)
+    graphicsA = new cax.Graphics()
+
+    graphicsA.beginPath().strokeStyle('white').lineWidth(2).arc(0, 0, 55, 0, Math.PI * 2).stroke()
+
+    graphicsA.x = 70
+    graphicsA.y = 70
+    groupA.add(bitmapA, graphicsA)
+    groupA.y = 200
+
+    groupA.compositeOperation = 'source-atop'
+
+
+    group.add(groupA)
+
+    group.cache(0, 0, 400, 800)
+    group.y = -200
+    stage.add(group)
+
+
+    cax.tick(() => {
+      stage.update()
+    })
+
+
+    document.querySelector('#btnA').addEventListener('click', () => {
+      group.y+=3
+      
+    })
+    
+    document.querySelector('#btnB').addEventListener('click', () => {
+      group.y-=3
+    })
+  }
+})
+
+loader.start()
---
+++ packages/avatar/src/index.js
@@ -0,0 +1,121 @@
+import cax from 'cax'
+const { Graphics, Text, Group } = cax
+
+class Axis extends Group {
+  constructor (axis, orient) {
+    super()
+    const scale = axis.scale
+    const f = scale.domain[0]
+    const t = scale.domain[1]
+    const rf = scale.range[0]
+    const rt = scale.range[1]
+
+    const x = axis.x
+    const y = axis.y
+    const g = new Graphics()
+    const moveTo = [0, 0]
+    const lineTo = [0, 0]
+    switch (orient) {
+      case 'left':
+      case 'right':
+        moveTo[0] = x 
+        moveTo[1] = y+ rf
+        lineTo[0] = x
+        lineTo[1] = y + rt
+        break
+      case 'top':
+      case 'bottom':
+        moveTo[0] = x + rf
+        moveTo[1] = y
+        lineTo[0] = x + rt
+        lineTo[1] = y
+        break
+    }
+
+    g.beginPath().strokeStyle(axis.color).moveTo( moveTo[0],  moveTo[1]).lineTo(lineTo[0], lineTo[1]).stroke()
+
+    let current
+    switch (orient) {
+      case 'bottom':
+        for (let i = f; i <= t; i += axis.interval) {
+          current = scale(i) + x
+          g.beginPath().strokeStyle(axis.color).moveTo(current, y).lineTo(current, y + 5).stroke()
+
+          if (axis.gird && i > f) {
+            g.beginPath().strokeStyle(axis.gird.color).moveTo(current, y - 1).lineTo(current, y - axis.gird.length).stroke()
+          }
+
+          if (!axis.text.range || i >= axis.text.range[0] && i <= axis.text.range[1]) {
+            const text = new Text(axis.text.value ? axis.text.value(i) : i, axis.text.font, axis.text.color)
+            text.x = current + axis.text.x
+            text.y = y + 5 + axis.text.y
+            text.rotation = axis.text.rotation || 0
+            this.add(text)
+          }
+        }
+        break
+      case 'left':
+
+        for (let i = f; i <= t; i += axis.interval) {
+       
+          current = scale(i)+y
+          g.beginPath().strokeStyle(axis.color).moveTo(x, current).lineTo(x - 5, current).stroke()
+
+          if (axis.gird && i > f) {
+            g.beginPath().strokeStyle(axis.gird.color).moveTo(x + 1, current).lineTo(x + axis.gird.length, current).stroke()
+          }
+          if (!axis.text.range || i >= axis.text.range[0] && i <= axis.text.range[1]) {
+            const text = new Text(axis.text.value ? axis.text.value(i) : i, axis.text.font, axis.text.color)
+            text.x = x - 5 + axis.text.x
+            text.y = current + axis.text.y
+            text.rotation = axis.text.rotation || 0
+            this.add(text)
+          }
+        }
+        break
+
+      case 'top':
+        for (let i = f; i <= t; i += axis.interval) {
+          
+          current = scale(i) + x
+          g.beginPath().strokeStyle(axis.color).moveTo(current, y).lineTo(current, y - 5).stroke()
+
+          if (axis.gird && i > f) {
+            g.beginPath().strokeStyle(axis.gird.color).moveTo(current, y - 1).lineTo(current, y - axis.gird.length).stroke()
+          }
+
+          if (!axis.text.range || i >= axis.text.range[0] && i <= axis.text.range[1]) {
+            const text = new Text(axis.text.value ? axis.text.value(i) : i, axis.text.font, axis.text.color)
+            text.x = current + axis.text.x
+            text.y = y - 5 + axis.text.y
+            text.rotation = axis.text.rotation || 0
+            this.add(text)
+          }
+        }
+        break
+
+      case 'right':
+
+        for (let i = f; i <= t; i += axis.interval) {
+          current = scale(i) + y
+          g.beginPath().strokeStyle(axis.color).moveTo(x, current).lineTo(x + 5, current).stroke()
+
+          if (axis.gird && i > f) {
+            g.beginPath().strokeStyle(axis.gird.color).moveTo(x + 1, current).lineTo(x + axis.gird.length, current).stroke()
+          }
+          if (!axis.text.range || i >= axis.text.range[0] && i <= axis.text.range[1]) {
+            const text = new Text(axis.text.value ? axis.text.value(i) : i, axis.text.font, axis.text.color)
+            text.x = x + 5 + axis.text.x
+            text.y = current + axis.text.y
+            text.rotation = axis.text.rotation || 0
+            this.add(text)
+          }
+        }
+        break
+    }
+
+    this.add(g)
+  }
+}
+
+export default Axis
