211af078d04d12de6c027bfe3a3b4695b022931e
dntzhang <dntzhang@qq.com>
2018-07-21 13:11:54 +0800

    fix excel merge bug

--- packages/excel/examples/simple/main.js
+++ packages/excel/examples/simple/main.js
@@ -12,13 +12,14 @@ const excel = new Excel([
   [3, 'merge main(show me)', 'merge2', null],
   [4, 'merge3', 'merge4', null],
   [5, null, null, null],
-  [6, null, null, null],
-  [7, 'center middle', 'top right', null]
+  [6, 'center middle', 'top right', null],
+  [7, 'Powered By Wechart & Cax & dntzhang & Tencent', null, null]
 ], {
   colWidth: [40, 200, 200, 130],
   rowHeight: [20, 30, 100, 30, 50, 60, 60, 60],
   merge: [
-    [1, 3, 2, 2]
+    [1, 3, 2, 2],
+    [1, 7, 3, 1]
   ],
   style: null
   // todo 自动标注顶部和左边,这里要自动多加一行和一列
@@ -35,8 +36,12 @@ excel.setBorder(1, 1, 'red')
 // 后设置覆盖前设置的
 excel.setBorder(1, 2, 'green')
 
-excel.style[7][2].textAlign = 'right'
-excel.style[7][2].verticalAlign = 'top'
+excel.style[6][2].textAlign = 'right'
+excel.style[6][2].verticalAlign = 'top'
+
+excel.style[7][1].color = 'red'
+excel.style[7][1].fontSize = '16'
+
 excel.update()
 
 stage.add(excel)
