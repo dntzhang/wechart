947b89290825d8b0405ae32ea5510fe1b1d3b294
张磊 <kmdjs@qq.com>
2018-07-26 15:49:28 +0800

    add astar

---
+++ packages/astar/app.js
@@ -0,0 +1,12 @@
+﻿var App = function () {
+    var canvas = document.querySelector("#ourCanvas"),ctx=canvas.getContext("2d");
+    var astarView = new AstarView(canvas);
+    var astarControl = new AstarController(astarView);
+    canvas.addEventListener("click", function (evt) {
+        var of = astarView.getOffset(canvas);
+        var x = Math.floor((evt.pageX - of[0] ) / astarView.dx);
+        var y = Math.floor((evt.pageY - of[1] ) / astarView.dy);
+        astarControl.input(x, y);
+    }, false);
+}
+
