230c9968675d469340217ca0ce3735c4ce9fedbf
daiwenqi <2540546236@qq.com>
2018-08-09 15:09:05 +0800

    block line

--- packages/music-visualization/examples/frequency2/main.js
+++ packages/music-visualization/examples/frequency2/main.js
@@ -119,12 +119,7 @@ loadAudio(media).then(buffer => {
   })
   scene.add(mesh3)
   mesh3.position.set(0, 0, -7)
-<<<<<<< HEAD
-  // ///////////////////////////////////////////////////////////
-=======
-  /// //////////////////////////////////////////////////////////
->>>>>>> upstream/master
-
+  
   ;(function animate () {
     window.requestAnimationFrame(animate)
     renderer.render(scene, camera)
